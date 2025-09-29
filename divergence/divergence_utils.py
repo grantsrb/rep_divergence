03c23fdeae60a69f5b4560f85d1e172213e19c4e
Satchel Grant <grantsrb@stanford.edu>
2025-09-29 10:47:53 -0700

    restructure for rep divergence project

--- divergence/divergence_utils.py
+++ divergence/divergence_utils.py
@@ -4,14 +4,36 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from geomloss import SamplesLoss
-import fca
+from fca.projections import perform_eigen_pca
 
 
 kwargs = { "loss": "sinkhorn", "p": 2, "blur": 0.05, }
 loss_fn = SamplesLoss(**kwargs)
 
 def compute_emd(X,Y):
-    return loss_fn(X,Y)
+    return loss_fn(X.float(),Y.float())
+
+def filter_by_layer_and_position(natty_hstates, intrv_hstates, layer=None, pos=None):
+    """
+    natty_hstates: torch tensor (B,Layer,Pos,D)
+    intrv_hstates: torch tensor (B,Layer,Pos,D)
+    layer: int
+    pos: int
+    """
+    d = natty_hstates.shape[-1]
+    if layer is not None:
+        natty_hstates = natty_hstates[:,layer]
+        intrv_hstates = intrv_hstates[:,layer]
+    if pos is None:
+        natty_states = [natty_hstates[i].reshape(-1,d) for i in range(len(natty_hstates))]
+        intrv_states = [intrv_hstates[i].reshape(-1,d) for i in range(len(natty_hstates))]
+    else:
+        natty_states = [natty_hstates[i][pos].reshape(-1,d) for i in range(len(natty_hstates))]
+        intrv_states = [intrv_hstates[i][pos].reshape(-1,d) for i in range(len(natty_hstates))]
+    natty_states = torch.vstack(natty_states)
+    intrv_states = torch.vstack(intrv_states)
+    return natty_states, intrv_states
+
 
 def sample_emd(X,Y, sample_type="identity", normalize=False, sample_size=None):
     """
@@ -28,14 +50,16 @@ def sample_emd(X,Y, sample_type="identity", normalize=False, sample_size=None):
     if sample_size is None: sample_size = len(X)
     sample_size = min(sample_size, len(X))
     if sample_type=="permute":
-        X = X[torch.randperm(sample_size).long()]
-        Y = Y[torch.randperm(sample_size).long()]
+        X = X[torch.randperm(len(X)).long()]
+        Y = Y[torch.randperm(len(X)).long()]
     elif sample_type=="sample":
-        X = X[torch.randint(0,len(X),(sample_size,)).long()]
-        Y = Y[torch.randint(0,len(Y),(sample_size,)).long()]
+        X = X[torch.randint(0,len(X),(len(X),)).long()]
+        Y = Y[torch.randint(0,len(Y),(len(X),)).long()]
     if normalize:
         X = (X-X.mean(0))/(X.std(0)+1e-5)
         Y = (Y-Y.mean(0))/(Y.std(0)+1e-5)
+    X = X[:sample_size]
+    Y = Y[:sample_size]
     return compute_emd(X, Y).item()
 
 def interleave_scatter(
@@ -66,56 +90,36 @@ def interleave_scatter(
     dark = 0.2,
     light = 0.85,
     rot = 0,
-    #intrv_cmap = sns.cubehelix_palette(start=-.3, rot=rot, dark=dark, light=light, reverse=True, as_cmap=True)
-    #color = None
-    #if hue is None:
-    #    color = intrv_cmap(0.7)
-    ##intrv_cmap = sns.dark_palette("blue", as_cmap=True)
+    
     intrv_cmap = sns.color_palette(intrv_palette)
     color = None
     if hue is None:
         color = sns.color_palette(intrv_palette)[0]
     sns.scatterplot(x="pc0", y="pc1", color=color, alpha=alpha, data=intrv_df, ax=ax, hue=hue, palette=intrv_cmap, edgecolor="none")
     
-    #native_cmap = sns.cubehelix_palette(start=0.7, rot=rot, dark=dark, light=light, reverse=True, as_cmap=True)
-    #if hue is None:
-    #    color = native_cmap(0.7)
-    ##native_cmap = sns.dark_palette("red", as_cmap=True)
     native_cmap = sns.color_palette(natty_palette)
     color = None
     if hue is None:
         color = sns.color_palette(natty_palette)[1]
     sns.scatterplot(x="pc0", y="pc1", color=color, alpha=alpha, data=natty_df, ax=ax, hue=hue, palette=native_cmap, edgecolor="none")
         
-    ## y divider
-    #ax.plot([0,0],[-1,5], "k--", alpha=0.5)
-    ## x dividers
-    #for i in y_values[:-1]:
-    #    y = i+0.5
-    #    ax.plot([-2,2],[y,y], "k--", alpha=0.5)
-    #plt.xlim([-2,2])
-    #plt.ylim([-0.75,4.75])
-    
     plt.xlabel("", fontsize=fontsize)
     plt.ylabel("", fontsize=fontsize)
     plt.xticks([], fontsize=fontsize)
     plt.yticks([], fontsize=fontsize)
     
-    # # Manually create colorbars / legend patches
-    # native_cmap = sns.cubehelix_palette(start=0.7, rot=rot, dark=dark, light=light, reverse=True, as_cmap=True)
-    # intrv_cmap = sns.cubehelix_palette(start=-.3, rot=rot, dark=dark, light=light, reverse=True, as_cmap=True)
-    
-    # Legend handles: colored rectangles with labels
-    native_patch = mpatches.Patch(color=color, label="Native")
-    intrv_patch = mpatches.Patch(color=color, label="Intervened")
     
     if not incl_legend:
         plt.legend().set_visible(False)
     else:
+        # Legend handles: colored rectangles with labels
+        color = sns.color_palette(natty_palette)[1]
+        native_patch = mpatches.Patch(color=color, label="Native")
+        color = sns.color_palette(intrv_palette)[0]
+        intrv_patch = mpatches.Patch(color=color, label="Intervened")
         ax.legend(handles=[native_patch, intrv_patch], fontsize=legendsize, loc="upper right", bbox_to_anchor=(1.75,1))
     if save_name:
         plt.savefig(save_name, dpi=600, bbox_inches="tight")
-        print("Saved to", save_name)
     
     plt.show()
 
@@ -127,26 +131,27 @@ def visualize_states(
     save_name=None,
     expl_var_threshold=0,
     sample_size=None,
-    sample_type="permute",
+    emd_sample_type="permute",
     sig_figs = 5,
     emd_sample_size=5000,
+    normalize_emd=True,
+    visualize=True,
+    verbose=True,
+    incl_legend=False,
 ):
 
     if sample_size:
         samp = torch.randperm(len(natty_states))[:sample_size].long()
         natty_states = natty_states[samp]
         intrv_states = intrv_states[samp]
-    print("Natty PCA")
+        
     X = np.concatenate([
         natty_states.clone().detach().cpu().float().numpy(),
         intrv_states.clone().detach().cpu().float().numpy(),
     ], axis=0)
     train_X = X
     
-    #pca = PCA()
-    #pca.fit(train_X)
-    #vecs = pca.transform(X)
-    ret = fca.projections.perform_eigen_pca(
+    ret = perform_eigen_pca(
         X=torch.tensor(train_X),
         scale=True,
         center=True,
@@ -158,10 +163,6 @@ def visualize_states(
     natty_vecs = torch.tensor(vecs[:len(natty_states)])
     intrv_vecs = torch.tensor(vecs[len(natty_states):])
     expl_vars = torch.tensor(ret["proportion_expl_var"]).float()
-    print("natty:", natty_vecs.shape)
-    print("intrv:", intrv_vecs.shape)
-    
-    print("Top Expl Vars:", expl_vars[:5])
     
     if xdim is None or ydim is None:
         diffs = ((natty_vecs-intrv_vecs)**2).mean(0)
@@ -173,36 +174,40 @@ def visualize_states(
         
     xdim = int(xdim)
     ydim = int(ydim)
-    print("X:", xdim, "Y:", ydim)
+    if verbose:
+        print("x-dim:", xdim)
+        print("y-dim:", ydim)
+        print("Top Expl Vars:", expl_vars[:5])
     
-    print("Expl Vars:",
-          ret["proportion_expl_var"][xdim],
-          ret["proportion_expl_var"][ydim]
-         )
+        print("Vis Expl Vars:",
+              ret["proportion_expl_var"][xdim],
+              ret["proportion_expl_var"][ydim]
+             )
     
-    natty_df = {
-        "pc0": natty_vecs[:,xdim],
-        "pc1": natty_vecs[:,ydim],
-        "hue": np.ones_like(natty_vecs[:,ydim]),
-    }
-    intrv_df = {
-        "pc0": intrv_vecs[:,xdim],
-        "pc1": intrv_vecs[:,ydim],
-        "hue": np.ones_like(natty_vecs[:,ydim]),
-    }
-    
-    interleave_scatter(
-        natty_df,
-        intrv_df,
-        hue=None,
-        title="", #f"Neural Space {varb}",
-        incl_legend=False,
-        save_name=save_name,
-    )
+    if visualize:
+        natty_df = {
+            "pc0": natty_vecs[:,xdim],
+            "pc1": natty_vecs[:,ydim],
+            "hue": np.ones_like(natty_vecs[:,ydim]),
+        }
+        intrv_df = {
+            "pc0": intrv_vecs[:,xdim],
+            "pc1": intrv_vecs[:,ydim],
+            "hue": np.ones_like(natty_vecs[:,ydim]),
+        }
+        
+        interleave_scatter(
+            natty_df,
+            intrv_df,
+            hue=None,
+            title="", #f"Neural Space {varb}",
+            incl_legend=incl_legend,
+            save_name=save_name,
+        )
     
     mse = ((natty_vecs-intrv_vecs)**2).mean().item()
-    emd = sample_emd(natty_vecs, intrv_vecs, sample_type=sample_type, normalize=False, sample_size=emd_sample_size)
-    base_emd = sample_emd(natty_vecs, natty_vecs, sample_type=sample_type, normalize=False, sample_size=emd_sample_size)
+    emd = sample_emd(natty_vecs, intrv_vecs, sample_type=emd_sample_type, normalize=normalize_emd, sample_size=emd_sample_size)
+    base_emd = sample_emd(natty_vecs, natty_vecs, sample_type=emd_sample_type, normalize=normalize_emd, sample_size=emd_sample_size)
     
     return {
         "mse": round(mse, sig_figs),
---
+++ divergence/toytask_utils.py
@@ -0,0 +1,323 @@
+import torch
+
+def make_tasks(task_division="distinct_y", varbs=None):
+    """
+    This function splits the classes into two different tasks.
+    
+    Args:
+        task_division: str
+            argue the type of task division
+            options:
+                distinct_y: task 1 and task 2 have 4 distinct y values with
+                    one example having a unique x value from the other 3 classes.
+                    Task one and task 2 have no overlapping examples.
+                distinct_y_overlap: task 1 and task 2 have 4 distinct y values with
+                    one example having a unique x value from the other 3 classes.
+                    Task one and task 2 have 2 overlapping examples.
+                distinct_xy: classes in task 1 and task 2 have distinct x and y
+                    values. None of the classes overlap between the two tasks.
+                distinct_xy_overlap: classes in task 1 and task 2 have distinct x
+                    and y values. Some of the classes overlap between the two tasks.
+                shared_y: task 1 and task 2 have 4 y values of which one is
+                    shared between two separate classes. None of the classes
+                    overlap between the two tasks.
+                shared_y_overlap: task 1 and task 2 have 4 y values of which one is
+                    shared between two separate classes. Some of the classes
+                    overlap between the two tasks.
+                tetris_L: task 1 and task 2 have 4 y values of which the top
+                    is shared between two separate classes for one task
+                    and the bottom is shared between two separate classes for
+                    the other task. The odd-one-out x value overlaps between
+                    the two tasks.
+                random: classes are split randomly between the two tasks. No
+                    overlap between the two tasks.
+                random_overlap: classes are split randomly between the two tasks.
+                    Some of the classes overlap between the two tasks.
+        varbs: ndarray or tensor
+            The variables to split into tasks. Assumes a two dimensional
+            tensor with x,y values in each row. Further, assumes x can
+            be -1 or 1, and y can be 0,1,2, 3, or 4.
+    """
+    #if task_division=="distinct_y":
+    #    task1_tups = {
+    #                (1,0),
+    #                (1,1),
+    #                (1,2),
+    #                (1,3),
+    #        (-1,4)
+    #    }
+    #    task2_tups = {
+    #                (1,4),
+    #        (-1,0),
+    #        (-1,1),
+    #        (-1,2),
+    #        (-1,3),
+    #    }
+    #elif task_division=="distinct_y_overlap":
+    #    task1_tups = {
+    #                (1,0),
+    #        (-1,1),
+    #                (1,2),
+    #                (1,3),
+    #        (-1,4),
+    #    }
+    #    task2_tups = {
+    #        (-1,0),
+    #        (-1,1),
+    #                (1,2),
+    #        (-1,2),
+    #        (-1,3),
+    #                (1,4),
+    #    }
+    #elif task_division=="distinct_xy":
+    #    task1_tups = {
+    #                (1,0),      
+    #        (-1,1), 
+    #    }
+    #    task2_tups = {
+
+
+    #        (-1,2), 
+    #                (1,3),      
+    #    }
+    #elif task_division=="distinct_xy_overlap":
+    #    task1_tups = {
+    #                (1,0),      
+    #        (-1,1), 
+    #    }
+    #    task2_tups = {
+
+    #        (-1,1), 
+    #                (1,2),      
+    #    }
+    #elif task_division=="xor":
+    #    task1_tups = {
+    #                (1,0),      
+    #        (-1,1), 
+    #    }
+    #    task2_tups = {
+    #        (-1,0), 
+    #               (1,1),      
+    #    }
+    if task_division=="original":
+        task1_tups = {
+            (-1,0), (1,0),
+            
+            (-1,2), (1,2),
+            (-1,3), (1,3),
+            (-1,4), (1,4),
+        }
+        task2_tups = {
+            (-1,0), (1,0),
+            (-1,1), (1,1),
+            
+            (-1,3), (1,3),
+            (-1,4), (1,4),
+        }
+    elif task_division=="shared_y":
+        task1_tups = {
+            (-1,0), (1,0),
+
+            (-1,2), (1,2),
+                    
+            (-1,4), (1,4),
+        }
+        task2_tups = {
+
+            (-1,1), (1,1),
+
+            (-1,3), (1,3),
+        }
+    elif task_division=="shared_y_overlap":
+        task1_tups = {
+            (-1,0), (1,0),
+
+            (-1,2), (1,2),
+            (-1,3), (1,3),
+        }
+        task2_tups = {
+
+            (-1,1), (1,1),
+            (-1,2), (1,2),
+
+            (-1,4), (1,4),
+        }
+    elif task_division=="tetris_L":
+        task1_tups = {
+            (-1,0), (1,0),
+                    (1,1),
+                    (1,2),
+                    (1,3),
+                    (1,4),
+        }
+        task2_tups = {
+            (-1,0),
+            (-1,1),
+            (-1,2),
+            (-1,3),
+            (-1,4), (1,4),
+        }
+    elif task_division=="mirror_L":
+        task1_tups = {
+            (-1,0), (1,0),
+                    (1,1),
+                    (1,2),
+                    (1,3),
+                    (1,4),
+        }
+        task2_tups = {
+            (-1,0), (1,0),
+            (-1,1),
+            (-1,2),
+            (-1,3),
+            (-1,4),
+        }
+    elif task_division=="tetris_T":
+        task1_tups = {
+                    (1,0),
+                    (1,1),
+            (-1,2), (1,2),
+                    (1,3),
+                    (1,4),
+        }
+        task2_tups = {
+            (-1,0),
+            (-1,1),
+            (-1,2), (1,2),
+            (-1,3),
+            (-1,4),
+        }
+    elif task_division=="tetris_T_no_overlap":
+        task1_tups = {
+                    (1,0),
+                    
+            (-1,2), (1,2),
+                    
+                    (1,4),
+        }
+        task2_tups = {
+
+            (-1,1),
+            (-1,2), (1,2),
+            (-1,3),
+        }
+    elif task_division=="tetris_C":
+        task1_tups = {
+            (-1,0), (1,0),
+                    (1,1),
+                    (1,2),
+                    (1,3),
+            (-1,4), (1,4),
+        }
+        task2_tups = {
+            (-1,0), (1,0),
+            (-1,1),
+            (-1,2),
+            (-1,3),
+            (-1,4), (1,4),
+        }
+    elif task_division=="tetris_h":
+        task1_tups = {
+            (-1,0),
+            (-1,1),
+            (-1,2), (1,2),
+            (-1,3), (1,3),
+            (-1,4), (1,4),
+        }
+        task2_tups = {
+            (-1,0), (1,0),
+            (-1,1), (1,1),
+            (-1,2), (1,2),
+                    (1,3),
+                    (1,4),
+        }
+    elif task_division=="mirror_h":
+        task1_tups = {
+            (-1,0),
+            (-1,1),
+            (-1,2), (1,2),
+            (-1,3), (1,3),
+            (-1,4), (1,4),
+        }
+        task2_tups = {
+                    (1,0),
+                    (1,1),
+            (-1,2), (1,2),
+            (-1,3), (1,3),
+            (-1,4), (1,4),
+        }
+    elif task_division=="tetris_F":
+        task1_tups = {
+            (-1,0), (1,0),
+            (-1,1),
+            (-1,2), (1,2),
+            (-1,3),
+            (-1,4),
+        }
+        task2_tups = {
+                    (1,0),
+                    (1,1),
+            (-1,2), (1,2),
+                    (1,3),
+            (-1,4), (1,4),
+        }
+    elif task_division=="mirror_F":
+        task1_tups = {
+            (-1,0), (1,0),
+            (-1,1),
+            (-1,2), (1,2),
+            (-1,3),
+            (-1,4),
+        }
+        task2_tups = {
+            (-1,0), (1,0),
+                    (1,1),
+            (-1,2), (1,2),
+                    (1,3),
+                    (1,4),
+        }
+    elif task_division=="inner_square":
+        task1_tups = {
+            (-1,0), (1,0),
+            
+            
+            (-1,3), (1,3),
+        }
+        task2_tups = {
+            
+            (-1,1), (1,1),
+            (-1,2), (1,2),
+        }
+    elif task_division=="random":
+        all_tups = {(x,y) for x in [-1,1] for y in range(5)}
+        task1_tups = {
+            tup for i,tup in enumerate(all_tups)
+                if i in torch.randperm(len(all_tups))[:len(all_tups)//2]
+        }
+        task2_tups = {tup for i,tup in enumerate(all_tups) if tup not in task1_tups}
+    elif task_division=="random_overlap":
+        all_tups = {(x,y) for x in [-1,1] for y in range(5)}
+        task1_tups = {tup for i,tup in enumerate(all_tups) if i in torch.randperm(len(all_tups))[:len(all_tups)//2]}
+        task2_tups = {tup for i,tup in enumerate(all_tups) if i in torch.randperm(len(all_tups))[len(all_tups)//2:]}
+    else:
+        raise ValueError(f"Invalid task division: {task_division}")
+    task1_bools = torch.tensor([(int(x),int(y)) in task1_tups for x,y in varbs])
+    task2_bools = torch.tensor([(int(x),int(y)) in task2_tups for x,y in varbs])
+    return task1_bools, task2_bools
+
+all_divisions = [
+    "shared_y",
+    "shared_y_overlap",
+    "tetris_L",
+    "mirror_L",
+    "tetris_T",
+    "tetris_T_no_overlap",
+    "tetris_C",
+    "tetris_h",
+    "mirror_h",
+    "tetris_F",
+    "mirror_F",
+    "random",
+    "random_overlap",
+    "inner_square",
+]
